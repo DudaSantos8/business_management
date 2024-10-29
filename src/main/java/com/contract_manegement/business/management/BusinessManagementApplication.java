62b08827908438e609bcf7c4ebaba985fb18b1d2
Duda <maria.oliveira@zup.com.br>
2024-10-29 11:10:52 -0300

    first commit

---
+++ src/main/java/com/contract_manegement/business/management/BusinessManagementApplication.java
@@ -0,0 +1,13 @@
+package com.contract_manegement.business.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contract_manegement/business/management/BusinessManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contract_manegement.business.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
